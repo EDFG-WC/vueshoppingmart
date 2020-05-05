72a89b312877494e98c69302411922d450928b54
EDFG-WC <wangchen706@hotmail.com>
2020-05-05 22:34:20 +0800

    5.5 commit P70

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,7 +1,15 @@
 <script src="../../router/index.js"></script>
 <template>
   <div>
-    <h3>权限列表</h3>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图-->
+    <el-card>123</el-card>
   </div>
 </template>
 
